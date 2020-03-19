1cf8c96fd3b0048cdbeb8fc408dc6ccb83234234
Conner Chappell <conner.chappell123@gmail.com>
2020-03-18 22:04:32 -0600

    Set up array and displayed its contents

--- unit05/js/main.js
+++ unit05/js/main.js
@@ -1,3 +1,18 @@
-document.querySelector('header > h1').innerText = 'The Arborist'
-document.querySelector('header > h2').innerText = 'Events and Arrays'
+document.querySelector('header > h1').textContent = 'The Arborist'
+document.querySelector('header > h2').textContent = 'Events and Arrays'
 
+// Create an array with 4 trees listed
+const trees = ['oak', 'Pine', 'Black Willow', 'aspen']
+const errorElement = document.querySelector('#error')
+const displayResults = document.querySelector('#displayResults')
+
+// Display the list of trees inside the displayResults div
+const listTrees = () => {
+    let treeList = ''
+    trees.forEach(tree => {
+        treeList += `${tree} <br>`
+    })
+    displayResults.innerHTML = `${treeList} <span>${trees.length} elements long</span>`
+} 
+
+listTrees()
