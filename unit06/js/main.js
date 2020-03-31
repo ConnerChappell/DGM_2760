c2d58e72ed01d76fc77f9ae3836cd66aa90ad2b2
Conner Chappell <conner.chappell123@gmail.com>
2020-03-30 20:47:42 -0600

    Initial javascript

--- unit06/js/main.js
+++ unit06/js/main.js
@@ -1,2 +1,12 @@
 document.querySelector('header > h1').textContent = 'Nonsense Story'
 document.querySelector('header > h2').textContent = 'String Manipulation'
+
+function tellStory() {
+    // nounArray = querySelector('#noun') //lowercase String split()
+    // repear for adjectives and verbs
+
+    const myStory = `Once upon a time there was a ${nounArray[0]} named Kirito...`
+
+
+    // get a reference to the querySelector('#story') = myStory
+}
