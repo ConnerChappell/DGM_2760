5afc93e88f93600856091cdf3c37d4e844f23bef
Conner Chappell <conner.chappell123@gmail.com>
2020-04-21 22:39:55 -0600

    trying to fix error

--- unit10/js/main.js
+++ unit10/js/main.js
@@ -3,7 +3,7 @@ document.querySelector('header > h2').textContent = 'Find Your Next Stay'
 
 async function getHotelData() {
     try {
-        const response = await fetch('hotel.json')
+        const response = await fetch("hotel.json")
         return await response.json()
     } catch (error) {
         console.error(error)
